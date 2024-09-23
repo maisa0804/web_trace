e821ccd3182361a35842b33d2524257e7c58bd4e
maisa0804 <breitmeer.0804@gmail.com>
2024-09-23 20:17:09 +0900

    Added style mag

---
+++ js/static/styleMag_data.js
@@ -0,0 +1,35 @@
+const styleMagData = [
+  {
+    img: "../../assets/images/stylemag/stylemag1.jpg",
+    description: "\ 一足お先に秋コーデ解禁 / 他の投稿はコチラから➤➤➤➤※これはダミーテキストです。"  
+  },
+  {
+    img: "../../assets/images/stylemag/stylemag2.jpg",
+    description: "\ 一足お先に秋コーデ解禁 / 他の投稿はコチラから➤➤➤➤※これはダミーテキストです。"  
+  },
+  {
+    img: "../../assets/images/stylemag/stylemag3.jpg",
+    description: "\ 一足お先に秋コーデ解禁 / 他の投稿はコチラから➤➤➤➤※これはダミーテキストです。"  
+  },
+  {
+    img: "../../assets/images/stylemag/stylemag4.jpg",
+    description: "\ 一足お先に秋コーデ解禁 / その他の投稿はコチラから➤➤➤➤※これはダミーテキストです。"
+  },
+  {
+    img: "../../assets/images/stylemag/stylemag5.jpg",
+    description: "\ 一足お先に秋コーデ解禁 / 他の投稿はコチラから➤➤➤➤※これはダミーテキストです。"  
+  },
+  {
+    img: "../../assets/images/stylemag/stylemag6.jpg",
+    description: "\ 一足お先に秋コーデ解禁 / 他の投稿はコチラから➤➤➤➤※これはダミーテキストです。"  
+  },
+  {
+    img: "../../assets/images/stylemag/stylemag7.jpg",
+    description: "\ 一足お先に秋コーデ解禁 / 他の投稿はコチラから➤➤➤➤※これはダミーテキストです。"  
+  },
+  {
+    img: "../../assets/images/stylemag/stylemag8.jpg",
+    description: "\ 一足お先に秋コーデ解禁 / 他の投稿はコチラから➤➤➤➤※これはダミーテキストです。"  
+  },
+];
+export default styleMagData;
